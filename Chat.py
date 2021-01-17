5b2d86b8a94e2d7c4611b54137d2354ab7353e92
mikezindel <mikezindel@hotmail.com>
2021-01-16 22:50:08 -0500

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,25 +1,21 @@
 #Python script to parse through text file
-date = 0
+
 file = open('WhatsApp Chat.txt', 'r') 
 thisdic = {}
 
 for line in file:
-	#parce out date
 	part = line.partition(',')
-	datenew = part[0]
-	if datenew != date:
-		date = datenew
-		#parce out part of the name from date
-		name = part[2]
-		name = name.partition('-')
-		#parce out name and message
-		name = name[2]
-		name = name.partition(':')
-		name = name[0].strip(" ")
-		#name and add counter value
-		if name in thisdic.keys():
-			thisdic[name] += 1
-		else:
-			thisdic[name] = 1
+	#parce out part of the name from date
+	name = part[2]
+	name = name.partition('-')
+	#parce out name and message
+	name = name[2]
+	name = name.partition(':')
+	name = name[0].strip(" ")
+	#name and add counter value
+	if name in thisdic.keys():
+		thisdic[name] += 1
+	else:
+		thisdic[name] = 1
 
 print(thisdic)
